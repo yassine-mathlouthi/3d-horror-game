55d60704d23c34deff3ed0ef8c9dfd9bf2b05450
yassine mathlouthi <yassinemt08@gmail.com>
2026-07-18 13:24:17 +0100

    new enemy

--- script/enemy/enemy.gd
+++ script/enemy/enemy.gd
@@ -11,12 +11,20 @@ const  WALKING_SPEED = 1.5
 func _ready() -> void:
 	player = get_node(player_path)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	velocity = Vector3.ZERO
+# Called every physics frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		velocity.y = 0.0
+
 	navigation_agent_3d.set_target_position(player.global_transform.origin)
 	var next_pos = navigation_agent_3d.get_next_path_position()
-	velocity = (next_pos - global_transform.origin).normalized() * RUNNING_SPEED
+	var direction = next_pos - global_position
+	direction.y = 0.0
+	direction = direction.normalized()
+	velocity.x = direction.x * RUNNING_SPEED
+	velocity.z = direction.z * RUNNING_SPEED
 	
 	look_at(
 		Vector3(
